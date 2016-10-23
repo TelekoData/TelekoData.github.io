21511108cd17b121e4460daa6d7329a1d33600e4
TelekoData <mattbaker@Matts-MBP.BigPond>
2016-10-23 12:56:03 +1100

    latest 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
+function onHover()
+{
+    $("#homeicon img").attr('src', 'img/home_white.svg');
+}
 
+function offHover()
+{
+    $("#homeicon img").attr('src', 'img/home_black.svg');
+}
+
+function onHover()
+{
+    $("#dataicon").attr('src', 'img/data_white.svg');
+}
+
+function offHover()
+{
+    $("#dataicon").attr('src', 'img/data_black.svg');
+}
+
+function onHover()
+{
+    $("#voiceicon").attr('src', 'img/voice_white.svg');
+}
+
+function offHover()
+{
+    $("#voiceicon").attr('src', 'img/voice_black.svg');
+}
+
+function onHover()
+{
+    $("#securityicon").attr('src', 'img/security_white.svg');
+}
+
+function offHover()
+{
+    $("#securityicon").attr('src', 'img/security_black.svg');
+}
+
+function onHover()
+{
+    $("#electricalicon").attr('src', 'img/electrical_white.svg');
+}
+
+function offHover()
+{
+    $("#electricalicon").attr('src', 'img/electrical_black.svg');
+}
+function onHover()
+{
+    $("#contacticon").attr('src', 'img/contact_white.svg');
+}
+
+function offHover()
+{
+    $("#contacticon").attr('src', 'img/contact_black.svg');
+}
