97ea4ead8aba04064dadbb02971325add8781a79
TelekoData <mattbaker@Matts-MacBook-Pro.local>
2016-10-24 20:52:22 +1100

    newest

--- js/main.js
+++ js/main.js
@@ -1,58 +1,42 @@
-function onHover()
-{
-    $("#homeicon img").attr('src', 'img/home_white.svg');
-}
+$(function(){
 
-function offHover()
-{
-    $("#homeicon img").attr('src', 'img/home_black.svg');
-}
+$("#homeicon").mouseover(function(){
+    $("#homeicon").attr('src', 'img/home_white.svg');
+}).mouseout(function(){
+    $("#homeicon").attr('src', 'img/home_black.svg');
+});
 
-function onHover()
-{
+$("#dataicon").mouseover(function(){
     $("#dataicon").attr('src', 'img/data_white.svg');
-}
-
-function offHover()
-{
+}).mouseout(function(){
     $("#dataicon").attr('src', 'img/data_black.svg');
-}
+});
 
-function onHover()
-{
+$("#voiceicon").mouseover(function(){
     $("#voiceicon").attr('src', 'img/voice_white.svg');
-}
-
-function offHover()
-{
+}).mouseout(function(){
     $("#voiceicon").attr('src', 'img/voice_black.svg');
-}
+});
 
-function onHover()
-{
+$("#securityicon").mouseover(function(){
     $("#securityicon").attr('src', 'img/security_white.svg');
-}
-
-function offHover()
-{
+}).mouseout(function(){
     $("#securityicon").attr('src', 'img/security_black.svg');
-}
+});
 
-function onHover()
-{
+$("#electricalicon").mouseover(function(){
     $("#electricalicon").attr('src', 'img/electrical_white.svg');
-}
-
-function offHover()
-{
+}).mouseout(function(){
     $("#electricalicon").attr('src', 'img/electrical_black.svg');
-}
-function onHover()
-{
-    $("#contacticon").attr('src', 'img/contact_white.svg');
-}
+});
 
-function offHover()
-{
+$("#contacticon").mouseover(function(){
+    $("#contacticon").attr('src', 'img/contact_white.svg');
+}).mouseout(function(){
     $("#contacticon").attr('src', 'img/contact_black.svg');
-}
+});
+
+
+
+
+});
